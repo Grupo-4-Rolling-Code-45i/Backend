47d613f4563d90bcf9faf508a3cc154504dfcc61
Máximo Majorel <maximomajorel@gmail.com>
2023-08-07 23:44:22 -0300

    Revert "Primer MVP backend (#15)"
    This reverts commit 85be70d380fac6996481e613dea6a6101f3f92e3.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Hola Mundo :D
-Rama development
--- app.js
+++ app.js
@@ -1,37 +1,14 @@
 // Importar Librerias
 const express = require("express");
-const { connectDB } = require("./database/config");
 require("dotenv").config();
 
 // Crear App
+
 const app = express();
 const PORT = process.env.PORT;
-const cors = require("cors");
-
-// Lectura y parseo del body
-app.use(express.json());
-
-// Directorio publico
-app.use(express.static("public"));
-
-
-
-// Usar CORS
-app.use(cors());
-
-// Conectar a la base de datos
-connectDB();
-
-// rutas
-app.use("/api/products/", require("./routes/products"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/users", require("./routes/users"));
-
-
-
-
 
 // Inicializar Servidor
+
 app.listen(PORT, () => {
   console.log(`Servidor Corriendo en el puerto: ${PORT}`);
 });
