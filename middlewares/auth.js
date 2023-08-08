bc80e1776d1c5611c81f84011980b89b750cedd4
Máximo Majorel <maximomajorel@gmail.com>
2023-08-08 00:19:33 -0300

    Development (#16)
    * Creación rama development
    
    * Estructura de carpetas
    
    * Configuración basica de MongoDB
    
    * se instalaron dependencias
    
    * Inclusión de CORS
    
    * se creo modelo producto
    
    * se creo modelo productoSeleccionado
    
    * se creo modelo pedido
    
    * se creo modelo usuario
    
    * se agregoel campo estado al modelo de susario (#4)
    
    * Feature/registro usuario (#3)
    
    * Crear usuarios en la base de datos con bcrypt
    
    * Generar JWT luego de registro
    
    * se creo ruta de editar usuario (#5)
    
    * correcion
    
    * Actualización mongoose v7.4.2
    
    mongoose version 7.4.0 > mongoose version 7.4.2
    
    * Feature/crear producto (#2)
    
    * Lógica basica post productos
    
    * Completado la logica de post de Crear Producto
    
    * Lógica eliminar producto (#7)
    
    * se agrego get de productos
    
    * Feature/editarusuario (#8)
    
    * se creo ruta de editar usuario
    
    * creacion de ruta users
    
    * ccrrecion de error
    
    * correcion e ruta
    
    * Lógica para solicitar todos los productos (#11)
    
    Me acepto el merge ya que esta funcionalidad fue probada en vivo en la Daily
    
    * Feature/login (#9)
    
    * Crear usuarios en la base de datos con bcrypt
    
    * Generar JWT luego de registro
    
    * Instalacion de dependencias, organizacion de carpetas y conexionexitosa con postman
    
    * Logica para encontrar mail y encriptar password
    
    * Logica de login comparar contraseña implementada
    
    * logica funcional
    
    * Lógica para solicitar todos los productos
    
    * correccion de conflictos
    
    * back-login funcional
    
    * agregue token
    
    * solucion de problemas
    
    * actualice .env
    
    ---------
    
    Co-authored-by: Máximo Majorel <maximomajorel@gmail.com>
    
    * se actualizaron funcionalidades
    
    * Feature/buscar productos por nombre (#13)
    
    * Logica obtener un producto
    
    * Logica de buscar producto
    
    * Vercel JSON
    
    * Test deploy
    
    * Prueba con index.js en vez de app.js
    
    * Cambios en el package.json para el deploy
    
    * Backend MVP funcionando
    
    ---------
    
    Co-authored-by: Damian Paz <damjpv@gmail.com>
    Co-authored-by: damianpazv <102426111+damianpazv@users.noreply.github.com>
    Co-authored-by: agustin-rubio <rubioagustin24@gmail.com>
    Co-authored-by: Nicolas Quinteros <106121751+nicoquinteros23@users.noreply.github.com>

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { Usuario } = require("../model/usuario");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    const { id } = jwt.verify(token, process.env.SECRET_JWT);
+    req.id = id;
+    
+    const user = await Usuario.findById(id);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = auth;
---
+++ model/productoSelec-model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const collection="productosSeleccionados";
+
+const productoSeleccionadoSchema = Schema({
+	nombre: {
+		type: String,
+		required: true,
+	},
+
+	precio: {
+		type: Number,
+		required: true,
+	},
+
+	cantidad: {
+		type: Number,
+		required: true,
+	},
+
+	descripcion: {
+		type: String,
+		required: true,
+	},
+
+	usuario: {
+		type: Schema.Types.ObjectId,
+		ref: 'Usuario',
+		required: true,
+	},
+});
+
+const ProductoSeleccionado=model(collection,productoSeleccionadoSchema);
+
+module.exports = {ProductoSeleccionado};
